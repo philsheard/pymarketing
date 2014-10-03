4c9a5f0980791ace7c8de20a3036e3615ddc582b
Phil Sheard <phil@philipsheard.co.uk>
2014-10-04 07:40:14 +1000

    Created a basic setup.py file, to test installation from github.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pymarketing',
+      version='0.1',
+      description='Python for Marketing',
+      url='https://github.com/philsheard/pymarketing',
+      author='Phil Sheard',
+      license='GNU GPL v2.0',
+      packages=['pymarketing'],
+      zip_safe=False)
